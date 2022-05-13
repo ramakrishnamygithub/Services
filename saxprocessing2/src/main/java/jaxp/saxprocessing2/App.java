784e95275bc53feb0c235316f9fe7a957438c054
ramakrishnamygithub <ramakrishnamogilipuri@gmail.com>
2022-05-13 07:49:41 +0530

    first commite

---
+++ saxprocessing2/src/main/java/jaxp/saxprocessing2/App.java
@@ -0,0 +1,13 @@
+package jaxp.saxprocessing2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
